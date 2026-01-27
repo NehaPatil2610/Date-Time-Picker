8acc16f0391d88a0f8ee8fddc1d85a921fa4ee6a
Neha Patil <114935107+NehaPatil2610@users.noreply.github.com>
2026-01-27 18:33:01 +0530

    Update main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,16 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y"],
-  framework: { name: "@storybook/react-vite", options: {} },
+  // Changed: Look in the root folder instead of /src/
+  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
 };
 export default config;
