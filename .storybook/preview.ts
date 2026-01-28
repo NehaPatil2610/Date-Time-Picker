82624228600bdcc32e0628985b85e9f2d781db77
Neha Patil <114935107+NehaPatil2610@users.noreply.github.com>
2026-01-28 20:57:20 +0530

    Update preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from "@storybook/react";
 // Changed: index.css is in the home folder, not /src/
-import "../index.css";
+import "../src/index.css";
 
 const preview: Preview = {
   parameters: {
